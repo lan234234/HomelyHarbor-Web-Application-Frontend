ff3c890a61ca402889714867c01841ae45cfbfd5
Lan <lan.peng234@gmail.com>
2023-08-06 15:44:30 -0700

    Add register() in utils.js

--- src/utils.js
+++ src/utils.js
@@ -20,6 +20,21 @@ export const login = (credential, asHost) => {
   });
 };
 
+export const register = (credential, asHost) => {
+  const registerUrl = `${domain}/register/${asHost ? "host" : "guest"}`;
+  return fetch(registerUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credential),
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw Error("Fail to register");
+    }
+  });
+};
+
 export const searchStays = (query) => {
   const authToken = localStorage.getItem("authToken");
   const searchStaysUrl = new URL(`${domain}/search/`);
