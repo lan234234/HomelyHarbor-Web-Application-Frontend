4ceb2e669138074a2ac27e630d2a75ee4c606486
Lan <lan.peng234@gmail.com>
2023-08-06 15:25:14 -0700

    Add login function in utils

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,21 @@
 const domain = "https://staybooking-394717.wm.r.appspot.com";
+
+export const login = (credential, asHost) => {
+  const loginUrl = `${domain}/authenticate/${asHost ? "host" : "guest"}`;
+
+  const requestStatus = fetch(loginUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(),
+  });
+
+  requestStatus.then((response) => {
+    if (response.status !== 200) {
+      throw Error("Fail to log in");
+    }
+
+    return response.json();
+  });
+};
