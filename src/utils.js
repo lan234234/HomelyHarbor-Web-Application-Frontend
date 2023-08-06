b702f296c55427237ec46a3951ca082f672834b3
Lan <lan.peng234@gmail.com>
2023-08-06 16:16:30 -0700

    Update login() in utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,16 +2,13 @@ const domain = "https://staybooking-394717.wm.r.appspot.com";
 
 export const login = (credential, asHost) => {
   const loginUrl = `${domain}/authenticate/${asHost ? "host" : "guest"}`;
-
-  const requestStatus = fetch(loginUrl, {
+  return fetch(loginUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(),
-  });
-
-  requestStatus.then((response) => {
+    body: JSON.stringify(credential),
+  }).then((response) => {
     if (response.status !== 200) {
       throw Error("Fail to log in");
     }
