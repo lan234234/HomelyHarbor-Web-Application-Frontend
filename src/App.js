90e60fcc4cfb1b807904551291c1a4c63c7fd470
Lan <lan.peng234@gmail.com>
2023-08-08 19:21:05 -0700

    import Layout from antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
 
 class App extends React.Component {
   render() {
