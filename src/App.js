f677bd6558bc97019697d1001473676d61583278
Lan <lan.peng234@gmail.com>
2023-08-08 19:17:24 -0700

    change App() component to class component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
-function App() {
-  return <></>;
+class App extends React.Component {
+  render() {
+    return <></>;
+  }
 }
 
 export default App;
