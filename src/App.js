8755c7b838c7153b994a295a5cf1bfd216259055
Lan <lan.peng234@gmail.com>
2023-08-08 20:23:54 -0700

    Change place holder to LoginPage component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Layout, Dropdown, Menu, Button } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import React from "react";
+import LoginPage from "./components/LoginPage";
 
 const { Header, Content } = Layout;
 
@@ -38,7 +39,7 @@ class App extends React.Component {
 
   renderContent = () => {
     if (!this.state.authed) {
-      return <div>login page</div>; //login page is just a place holder
+      return <LoginPage />;
     }
 
     if (this.state.asHost) {
