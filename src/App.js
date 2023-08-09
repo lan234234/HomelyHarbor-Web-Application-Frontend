07c2a04db912bd2eecc07803052e9b90f22b5585
Lan <lan.peng234@gmail.com>
2023-08-08 20:13:29 -0700

    Import Menu and Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Layout, Dropdown, Menu, Button } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import React from "react";
-import { Layout } from "antd";
 
 const { Header, Content } = Layout;
 
