f6a4ab5f5beeab8c4b7cdfba683848e3a898aad8
Lan <lan.peng234@gmail.com>
2023-08-10 19:18:41 -0700

    Add basic layout in HostHomePage

--- src/components/HostHomePgae.js
+++ src/components/HostHomePgae.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Tabs } from "antd";
 
 class HostHomePage extends React.Component {
   render() {
-    return <></>;
+    return (
+      <Tabs>
+        <Tabs.TabPane>My Stays</Tabs.TabPane>
+        <Tabs.TabPane>Upload Stays</Tabs.TabPane>
+      </Tabs>
+    );
   }
 }
 
