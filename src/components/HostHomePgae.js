c050376644c574ae95fad16a77f04c28279de4cf
Lan <lan.peng234@gmail.com>
2023-08-10 19:34:30 -0700

    Destructuring TabPane from Tabs

--- src/components/HostHomePgae.js
+++ src/components/HostHomePgae.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Tabs } from "antd";
 
+const { TabPane } = Tabs;
+
 class HostHomePage extends React.Component {
   render() {
     return (
-      <Tabs defaultActiveKey="2">
-        <Tabs.TabPane tab="My Stays" key="1">
-          My Stays
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Upload Stay" key="2">
-          Upload Stays
-        </Tabs.TabPane>
+      <Tabs defaultActiveKey="1" destroyInactiveTabPane={true}>
+        <TabPane tab="My Stays" key="1">
+          <div>My Stays Content</div>
+        </TabPane>
+        <TabPane tab="Upload Stay" key="2">
+          <div>Upload Stays</div>
+        </TabPane>
       </Tabs>
     );
   }
