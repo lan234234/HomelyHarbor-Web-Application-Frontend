1c1f1bd3b73a001d266da95937877354666dd797
Lan <lan.peng234@gmail.com>
2023-08-10 19:24:05 -0700

    Add tab name and set defaultActiveKey for tab

--- src/components/HostHomePgae.js
+++ src/components/HostHomePgae.js
@@ -4,9 +4,13 @@ import { Tabs } from "antd";
 class HostHomePage extends React.Component {
   render() {
     return (
-      <Tabs>
-        <Tabs.TabPane>My Stays</Tabs.TabPane>
-        <Tabs.TabPane>Upload Stays</Tabs.TabPane>
+      <Tabs defaultActiveKey="2">
+        <Tabs.TabPane tab="My Stays" key="1">
+          My Stays
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Upload Stay" key="2">
+          Upload Stays
+        </Tabs.TabPane>
       </Tabs>
     );
   }
