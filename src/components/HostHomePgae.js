26ad42f621ff1b7588ee5424d458de5fc5ac3a7c
Lan <lan.peng234@gmail.com>
2023-08-12 10:17:30 -0700

    Add handleRemoveStay() in RemoveStayButton

--- src/components/HostHomePgae.js
+++ src/components/HostHomePgae.js
@@ -27,6 +27,25 @@ export class RemoveStayButton extends React.Component {
     loading: false,
   };
 
+  handleRemoveStay = async () => {
+    const { stay, onRemoveSuccess } = this.props;
+
+    this.setState({
+      loading: true,
+    });
+
+    try {
+      await deleteStay(stay.id);
+      onRemoveSuccess();
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
   render() {
     return <></>;
   }
