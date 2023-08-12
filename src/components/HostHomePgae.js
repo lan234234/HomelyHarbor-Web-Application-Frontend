e2406ebc70276bcc08434681ab6810df1b01da2f
Lan <lan.peng234@gmail.com>
2023-08-12 10:38:22 -0700

    Add ReservationList logic

--- src/components/HostHomePgae.js
+++ src/components/HostHomePgae.js
@@ -22,11 +22,58 @@ import UploadStay from "./UploadStay";
 
 const { TabPane } = Tabs;
 
-export class ReservationList extends React.Component {
-  state = {};
+class ReservationList extends React.Component {
+  state = {
+    loading: false,
+    reservations: [],
+  };
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = async () => {
+    this.setState({
+      loading: true,
+    });
+
+    try {
+      const resp = await getReservationsByStay(this.props.stayId);
+      this.setState({
+        reservations: resp,
+      });
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
 
   render() {
-    return <></>;
+    const { loading, reservations } = this.state;
+
+    return (
+      <List
+        loading={loading}
+        dataSource={reservations}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              title={<Text>Guest Name: {item.guest.username}</Text>}
+              description={
+                <>
+                  <Text>Checkin Date: {item.checkin_date}</Text>
+                  <br />
+                  <Text>Checkout Date: {item.checkout_date}</Text>
+                </>
+              }
+            />
+          </List.Item>
+        )}
+      />
+    );
   }
 }
 
