1051a98b556cf295e3a6885b66cd9d04139db835
Lan <lan.peng234@gmail.com>
2023-08-12 10:45:22 -0700

    Add ViewReservationsButton component

--- src/components/HostHomePgae.js
+++ src/components/HostHomePgae.js
@@ -77,6 +77,52 @@ class ReservationList extends React.Component {
   }
 }
 
+class ViewReservationsButton extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  openModal = () => {
+    this.setState({
+      modalVisible: true,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      modalVisible: false,
+    });
+  };
+
+  render() {
+    const { stay } = this.props;
+    const { modalVisible } = this.state;
+
+    const modalTitle = `Reservations of ${stay.name}`;
+
+    return (
+      <>
+        <Button onClick={this.openModal} shape="round">
+          View Reservations
+        </Button>
+        {modalVisible && (
+          <Modal
+            title={modalTitle}
+            centered={true}
+            visible={modalVisible}
+            closable={false}
+            footer={null}
+            onCancel={this.handleCancel}
+            destroyOnClose={true}
+          >
+            <ReservationList stayId={stay.id} />
+          </Modal>
+        )}
+      </>
+    );
+  }
+}
+
 export class RemoveStayButton extends React.Component {
   state = {
     loading: false,
