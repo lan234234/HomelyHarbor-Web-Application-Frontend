5490858ff78eb2b5bf82bb117198ede9fab3f524
Lan <lan.peng234@gmail.com>
2023-08-12 14:17:18 -0700

    Add main layout

--- src/components/GuestHomePage.js
+++ src/components/GuestHomePage.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Tabs } from "antd";
+
+const { TabPane } = Tabs;
 
 class GuestHomePage extends React.Component {
   render() {
-    return <></>;
+    return (
+      <Tabs defaultActiveKey="1" destroyInactiveTabPane={true}>
+        <TabPane tab="Search Stays" key="1">
+          Search Stays Content
+        </TabPane>
+        <TabPane tab="My Reservations" key="2">
+          My Reservations Content
+        </TabPane>
+      </Tabs>
+    );
   }
 }
 
