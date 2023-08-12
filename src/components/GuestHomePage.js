507d053dc59998b58aefbd256be1c12a4cdba2cb
Lan <lan.peng234@gmail.com>
2023-08-12 15:30:13 -0700

    Add basic layout of BookStayButton

--- src/components/GuestHomePage.js
+++ src/components/GuestHomePage.js
@@ -20,8 +20,58 @@ import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons";
 const { TabPane } = Tabs;
 
 class BookStayButton extends React.Component {
+  state = {
+    loading: false,
+    modalVisible: false,
+  };
+
   render() {
-    return <></>;
+    const { stay } = this.props;
+    return (
+      <>
+        <Button onClick={this.handleBookStay} shape="round" type="primary">
+          Book Stay
+        </Button>
+        <Modal
+          destroyOnClose={true}
+          title={stay.name}
+          visible={this.state.modalVisible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <Form
+            preserve={false}
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            onFinish={this.handleSubmit}
+          >
+            <Form.Item
+              label="Checkin Date"
+              name="checkin_date"
+              rules={[{ required: true }]}
+            >
+              <DatePicker />
+            </Form.Item>
+            <Form.Item
+              label="Checkout Date"
+              name="checkout_date"
+              rules={[{ required: true }]}
+            >
+              <DatePicker />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button
+                loading={this.state.loading}
+                type="primary"
+                htmlType="submit"
+              >
+                Book
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
+      </>
+    );
   }
 }
 
